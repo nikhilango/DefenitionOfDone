2f990ce24e9dbf8fbf7a5fb67d75c2f38692fc2b
nikhilango <nikhil.ilango@noseryoung.com>
2022-10-05 16:27:24 +0200

    second final commit

--- badProgram/main.c
+++ badProgram/main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]){
-    char input[]="Hello mah friend \n";
-    int size=16;
-    printf("Your input reversed is: ");
-    while(size>=0){
-        printf("%c", input[size]);
-        size--;}
-    return 0;
+char iNpUt[]="Hello mah friend \n";
+int SiZE=16;// 
+printf("Your input reversed is: ");
+while(SiZE>=0){
+printf("%c", iNpUt[SiZE]);
+SiZE--;}
+return 0;
 }
